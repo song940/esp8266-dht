c04d9829a6caef329a26e925553ab85b2e7f4545
Lsong <song940@gmail.com>
2023-11-01 17:28:48 +0800

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# ESP8266 DHT
+
+![](https://m.media-amazon.com/images/I/514KLFg67gL.jpg)
+
+<https://lsong.org/notes/esp8266.html>
--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,31 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiManager.h>
+#include <DHT.h>
 
 // WiFiManager settings
 char mqttServer[40] = "192.168.8.160"; // Default MQTT server
-char mqttPort[6] = "1883"; // Default MQTT port
-char mqttUser[40] = ""; // Default MQTT user (can be an empty string)
-char mqttPassword[40] = ""; // Default MQTT password (can be an empty string)
+char mqttPort[6] = "1883";             // Default MQTT port
+char mqttUser[40] = "";                // Default MQTT user (can be an empty string)
+char mqttPassword[40] = "";            // Default MQTT password (can be an empty string)
 
-const char* apName = "ESP8266-MQTT";
-const char* clientID = "ESP8266-LED";
-const char* ledTopic = "led";
+const char *apName = "ESP8266-DHT";
+const char *clientID = "ESP8266-DHT";
+const char *ledTopic = "led";
+const char *dhtTopic = "dht";
 const int ledPin = LED_BUILTIN;
+unsigned long lastTransmissionTime = 0;
 
+#define DHT_PIN 2
+#define DHT_TYPE DHT22
+
+DHT dht(DHT_PIN, DHT_TYPE);
 WiFiClient wlan;
 PubSubClient mqtt(wlan);
 
 // WiFiManager callback function
-void saveConfigCallback() {
+void saveConfigCallback()
+{
   Serial.println("Configuration saved");
   strcpy(mqttServer, WiFiManagerParameter("mqtt_server").getValue());
   strcpy(mqttPort, WiFiManagerParameter("mqtt_port").getValue());
@@ -26,43 +34,56 @@ void saveConfigCallback() {
   strcpy(mqttPassword, WiFiManagerParameter("mqtt_password").getValue());
 }
 
-void reconnect() {
-  while (!mqtt.connected()) {
+void reconnect()
+{
+  while (!mqtt.connected())
+  {
     Serial.println("Attempting to connect to the MQTT server...");
-    if (mqtt.connect(clientID, mqttUser, mqttPassword)) {
+    if (mqtt.connect(clientID, mqttUser, mqttPassword))
+    {
       Serial.println("MQTT reconnected");
       mqtt.subscribe(ledTopic);
-    } else {
+    }
+    else
+    {
       Serial.print("MQTT connect failed, retrying...");
       delay(2000);
     }
   }
 }
 
-void onMessage(char* topic, byte* payload, unsigned int length) {
-  // Handle received messages
-  String payloadStr = "";
-  for (int i = 0; i < length; i++) {
-    payloadStr += (char)payload[i];
+void readDHT()
+{
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (!isnan(humidity) && !isnan(temperature))
+  {
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %, Temp: ");
+    Serial.print(temperature);
+    Serial.println(" Celsius");
+
+    // Format the data as a JSON object
+    String json = "{\"humidity\":" + String(humidity, 2) + ", \"temperature\":" + String(temperature, 2) + "}";
+
+    // Publish the JSON data to the MQTT topic
+    mqtt.publish(dhtTopic, json.c_str());
+
+    // Update the last transmission time
+    lastTransmissionTime = millis();
   }
-
-  Serial.print("Received message: [");
-  Serial.print(topic);
-  Serial.print("] ");
-  Serial.println(payloadStr);
-
-  if (strcmp(topic, ledTopic) == 0) {
-    // If an LED control message is received, perform the corresponding action
-    if (payloadStr == "on") {
-      digitalWrite(ledPin, HIGH);
-    } else if (payloadStr == "off") {
-      digitalWrite(ledPin, LOW);
-    }
+  else
+  {
+    Serial.println("Failed to read from DHT sensor!");
   }
 }
 
-
-void setup() {
+void setup()
+{
+  dht.begin();
+  Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
 
   WiFiManager wifiManager;
@@ -83,13 +104,19 @@ void setup() {
 
   // Set the MQTT server and port
   mqtt.setServer(mqttServer, atoi(mqttPort));
-  mqtt.setCallback(onMessage);
   mqtt.setClient(wlan);
 }
 
-void loop() {
-  if (!mqtt.connected()) {
+void loop()
+{
+  if (!mqtt.connected())
+  {
     reconnect();
   }
   mqtt.loop();
+
+  if (millis() - lastTransmissionTime >= 1000)
+  {
+    readDHT();
+  }
 }
